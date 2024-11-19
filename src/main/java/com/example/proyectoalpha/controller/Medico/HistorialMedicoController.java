e0eb380c9f9a5bcb40fbaf9186353d02c28e4d24
Daniel Lara <d.laraalcaraz@gmail.com>
2024-11-19 19:03:18 +0100

    @Autor: Daniel Lara Realización ventana monitorización de salud

--- src/main/java/com/example/proyectoalpha/controller/Medico/HistorialMedicoController.java
+++ src/main/java/com/example/proyectoalpha/controller/Medico/HistorialMedicoController.java
@@ -1,4 +1,45 @@
 package com.example.proyectoalpha.controller.Medico;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
 public class HistorialMedicoController {
+
+    @FXML
+    private ImageView LblTitulo;
+
+    @FXML
+    private ImageView GPS;
+
+    @FXML
+    private ImageView O2;
+
+    @FXML
+    private ImageView FC;
+
+    @FXML
+    private Label LblGpsData;
+
+    @FXML
+    private Label LblO2;
+
+    @FXML
+    private Label LblFC;
+
+    @FXML
+    private TextField BuscarTxt;
+
+    @FXML
+    private Button BtnMostrar;
+
+    @FXML
+    private Label LblUrgencia;
+
+    @FXML
+    private Button BtnVolver;
+
 }
+
