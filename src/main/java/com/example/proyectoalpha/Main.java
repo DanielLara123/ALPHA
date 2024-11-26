bca379b393e86d3467a2e78b93e298c52cef8a6a
jaime-515 <125456543+jaime-515@users.noreply.github.com>
2024-11-26 18:53:48 +0100

    @author: Jaime Fernández: Reajuste resolución ventanas al ser abiertas o llamadas (formato 1080x720)

--- src/main/java/com/example/proyectoalpha/Main.java
+++ src/main/java/com/example/proyectoalpha/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("TipoUsuario.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1080, 720);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
         stage.show();
     }
