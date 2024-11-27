dc683a0ef813eb506a52df6d3dfac58b4c6d36f8
Daniel Lara <d.laraalcaraz@gmail.com>
2024-11-27 12:09:20 +0100

    @Autor: Daniel Lara Cambios en archivo fxml y controlador de mis datos

--- src/main/java/com/example/proyectoalpha/controller/Atleta/MisDatosUsuarioController.java
+++ src/main/java/com/example/proyectoalpha/controller/Atleta/MisDatosUsuarioController.java
@@ -1,4 +1,26 @@
 package com.example.proyectoalpha.controller.Atleta;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class MisDatosUsuarioController {
+
+    @FXML
+    private Label LblCorreo;
+
+    @FXML
+    private Label LblNombre;
+
+    @FXML
+    private Label LblTelefono;
+
+    @FXML
+    private Label LblSetCorreo;
+
+    @FXML
+    private Label LblSetNombre;
+
+    @FXML
+    private Label LblSetTlf;
+
 }
